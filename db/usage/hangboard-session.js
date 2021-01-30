bdea0af7a9d18374b5e21624850bc9197ce0e700
michaelfswann <mikeswann@mail.com>
2021-01-30 23:26:15 +0000

    added mongoose, hangboard session model and schema

---
+++ db/usage/hangboard-session.js
@@ -0,0 +1,5 @@
+//Create a HangboardSessionModel model just by requiring the module
+var HangboardSessionModel = require("../models/hangboard-session");
+
+// Use the SomeModel object (model) to find all SomeModel records
+HangboardSessionModel.find(callback_function);

10a6085893b2644913435f4c7a1fbb8a8dcc1bbb
michaelfswann <mikeswann@mail.com>
2021-02-02 17:44:24 +0000

    added some comments

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const MongoClient = require("mongodb").MongoClient;
 const URL = process.env.MONGO_DB_CONNECTION_STRING;
 
+// connect to database
 MongoClient.connect(URL, { useUnifiedTopology: true })
   .then((client) => {
     const db = client.db("hangboard_sessions");
 
+    // respond to get requests at /mongo/ with all documents in collection
     router.get("/", function (req, res) {
-      // do something
       db.collection("hangboard_sessions_collection")
         .find()
         .toArray()
